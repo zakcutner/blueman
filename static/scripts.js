5420819720627a26e1de8b09993ae96ef6627953
Zak Cutner <me@zakcutner.uk>
2015-11-22 09:58:08 +0000

    Client accepts statements

--- static/scripts.js
+++ static/scripts.js
@@ -8,13 +8,38 @@ var question = {
 
         $('nav').prepend('<span>' + counter + '</span>');
         $('.question p').html(details.text);
-        $('.question img').attr('src', '/static/' + details.image);
+        $('.question img').attr('src', details.image);
     },
 
     submit: function(answer) {
         $.post('/api/', {uuid: uuid, question_id: this.question, answer: answer}, function(data) {
-            question.init(data.question);
+            if(data.question) question.init(data.question);
+            else if(data.statement) statement.init(data.statement);
         });
+    }
+};
+
+var statement = {
+    init: function(details) {
+        counter++;
+
+        $('nav').prepend('<span>' + counter + '</span>');
+        $('.question p').html(details.text);
+
+        if(details.image) $('.question img').attr('src', details.image);
+        else $('.question img').hide();
+
+        $('#yes, #no').hide();
+        $('#restart').show();
+    },
+
+    submit: function() {
+        $('.question.img').removeAttr('src').show();
+        $('#yes, #no').show();
+        $('#restart').hide();
+        $('nav').empty();
+        counter = 0;
+        init();
     }
 };
 
@@ -28,19 +53,26 @@ $('#no').click(function(e) {
     question.submit(false);
 });
 
-(function() {
+$('#restart').click(function(e) {
+    e.preventDefault();
+    statement.submit();
+});
+
+function init() {
     if('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(function(position) {
             $.post('/api/', {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             }, function(data) {
-                console.log(data);
                 uuid = data.uuid;
-                question.init(data.question);
+                if(data.question) question.init(data.question);
+                else if(data.statement) statement.init(data.statement);
             });
         });
     } else {
         console.log('Geolocation has been blocked or is not supported on this device!');
     }
-})();
+}
+
+init();
